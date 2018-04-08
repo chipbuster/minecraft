9b9dc1f5c8dd58e6594d99c17af165c0281d1f80
Kevin Song <chipbuster@gmail.com>
2018-04-08 18:35:10 -0500

    Progress on collision--still broken

--- src/camera.cc
+++ src/camera.cc
@@ -1,11 +1,14 @@
 #include "camera.h"
+#include <cmath>
+#include <algorithm>
 #include <glm/gtx/rotate_vector.hpp>
 #include <glm/gtx/string_cast.hpp>
-#include <cmath>
 
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::max;
+using std::min;
 
 std::ostream& operator<<(std::ostream& os, const glm::vec4 x);
 std::ostream& operator<<(std::ostream& os, const glm::vec3 x);
@@ -33,7 +36,7 @@ void Camera::lm_rotate_cam(double screendX, double screendY)
     glm::vec2 amt = glm::normalize(glm::dvec2(screendX, screendY));
 
     // The input from screen space is nonsensical. Ignore it.
-    if(std::isnan(glm::length(amt))){
+    if (std::isnan(glm::length(amt))) {
         return;
     }
 
@@ -42,16 +45,17 @@ void Camera::lm_rotate_cam(double screendX, double screendY)
 
     glm::mat4 rotation = glm::rotate(rotation_speed, rotAxis);
 
-    glm::vec3 nextLook = glm::normalize(glm::vec3(rotation * glm::vec4(this->look_,1.0f)));
-    if(glm::dot(nextLook, this->true_up_) > 0.99){
+    glm::vec3 nextLook =
+            glm::normalize(glm::vec3(rotation * glm::vec4(this->look_, 1.0f)));
+    if (glm::dot(nextLook, this->true_up_) > 0.99) {
         this->look_ = this->look_;
-    }else{
+    } else {
         this->look_ = nextLook;
         // Rule: look, up_, and true_up_ should be collinear
-        this->up_ = glm::normalize(
-            this->true_up_ - glm::dot(this->look_, this->true_up_) * this->look_
-        );
-    }   
+        this->up_ = glm::normalize(this->true_up_ -
+                                   glm::dot(this->look_, this->true_up_) *
+                                           this->look_);
+    }
 
     assert(fabs(glm::dot(this->up_, this->look_)) < 0.01);
 
@@ -82,15 +86,13 @@ void Camera::mm_trans_cam(double screendX, double screendY)
 
 void Camera::ws_walk_cam(int direction)
 {
-    if(physics_mode){
-        if (direction > 0){
+    if (physics_mode) {
+        if (direction > 0) {
             velocity_ += look_;
-        }
-        else{
+        } else {
             velocity_ -= look_;
         }
-    }
-    else{
+    } else {
         if (direction > 0) {
             eye_ += zoom_speed * look_;
         } else {
@@ -122,7 +124,7 @@ void Camera::update_internal_data()
     glm::mat3 rot(camX, camY, camZ);
 
     glm::mat4 cam_to_world = glm::mat4(rot);
-    cam_to_world[3] = glm::vec4(eye_,1.0f);
+    cam_to_world[3] = glm::vec4(eye_, 1.0f);
 
     this->right_ = camX;
     this->cam_to_world_ = cam_to_world;
@@ -130,38 +132,140 @@ void Camera::update_internal_data()
     this->center_ = eye_ + camera_distance_ * look_;
 }
 
-void Camera::update_physics(double timestep, const Chunk& C, const std::vector<glm::vec3>& cubes){
+enum CollisionType {
+    NONE = 0, 
+    CEIL = 1,
+    FLOOR = 2,
+    MINX = 4,
+    MAXX = 8,
+    MINZ = 16,
+    MAXZ = 32,
+    WALL = 64
+};
+inline CollisionType operator|(CollisionType a, CollisionType b)
+{return static_cast<CollisionType>(static_cast<int>(a) | static_cast<int>(b));}
+inline CollisionType operator&(CollisionType a, CollisionType b)
+{return static_cast<CollisionType>(static_cast<int>(a) & static_cast<int>(b));}
+
+
+
+CollisionType collide(const glm::vec3& location, const glm::vec3& Cmin)
+{
+    constexpr float camH = 1.75;
+    constexpr float camR = 0.5;
+
+    float minX = Cmin.x;
+    float maxX = minX + 1.0;
+    float minZ = Cmin.z;
+    float maxZ = minZ + 1.0;
+    float minY = Cmin.y;
+    float maxY = Cmin.y + 1.0;
+
+    float camBot = location.y - camH;
+    float camTop = location.y;
+    float camMinX = location.x - camR;
+    float camMaxX = location.x + camR;
+    float camMinZ = location.z - camR;
+    float camMaxZ = location.z + camR;
+
+    CollisionType ct = NONE;
+
+
+    float eps = 0.10;
+
+    bool MaxYCross = camBot < maxY + eps && camTop > maxY; // Camera crosses maxY
+    bool MinYCross = camTop > minY - eps && camBot < minY; // Camera crosses minY
+    bool MaxXCross = camMinX < maxX + eps && camMaxX > maxX;
+    bool MaxZCross = camMinZ < maxZ + eps && camMaxZ > maxZ;
+    bool MinXCross = camMaxX > minX - eps && camMinX < minX;
+    bool MinZCross = camMaxZ > minZ - eps && camMinZ < minZ;
+
+    // Cylinder is inbounds if it crosses an edge or is entirely within the cell
+    bool inBoundsY = MaxYCross || MinYCross || camBot > minY && camTop < maxY;
+    bool inBoundsX = MaxXCross || MinXCross || camMaxX < maxX && camMinX > minX;
+    bool inBoundsZ = MaxZCross || MinZCross || camMaxZ < maxZ && camMinZ > minZ;
+
+    if(MaxYCross && inBoundsX && inBoundsZ){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type FLOOR" <<std::endl;
+        ct = ct | FLOOR;
+    }
+    if(MinYCross && inBoundsX && inBoundsZ){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type CEIL"<< std::endl;
+        ct = ct | CEIL;
+    }
+    if(MaxXCross && inBoundsY && inBoundsZ){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type MINX" <<std::endl;
+        ct = ct | MINX;
+    }
+    if(MinXCross && inBoundsY && inBoundsZ){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type MAXX" <<std::endl;
+        ct = ct | MAXX;
+    }
+    if(MaxXCross && inBoundsY && inBoundsX){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type MINZ" <<std::endl;
+        ct = ct | MINZ;
+    }
+    if(MinXCross && inBoundsY && inBoundsX){
+        cout << "Collision of " << glm::to_string(location) << " with " << glm::to_string(Cmin) << " of type MAXZ"<< std::endl;
+        ct = ct | MAXZ;
+    }
+    return ct;
+}
+
+void Camera::update_physics(double timestep, const Chunk& C,
+                            const std::vector<glm::vec3>& cubes)
+{
     constexpr float camH = 1.75;
     constexpr float camR = 0.5;
     // Update camera velocity from gravity
-    this->velocity_ += glm::vec3(0.0f,-gravity, 0.0f);
+    this->velocity_ += glm::vec3(0.0f, -gravity, 0.0f);
 
     // Update camera velocity from friction
     this->velocity_ *= 0.8;
 
     // Update camera velocity from collisions
-    std::vector<glm::vec3> collisions; // Cube indices that we might collide with
-    
+    std::vector<glm::vec3>
+            coarseCollisions; // Cube indices that we might collide with
+
     // Coarse detection
     constexpr float coarseRadius = 1.5 + sqrt(camR * camR + camH * camH);
-    for(const auto& c : cubes){
-        if(glm::length(c - this->eye_) < coarseRadius){
-            collisions.push_back(c);
-        }
-    }
-    // Fine detection: floor
-    for(const auto& c : collisions){
-        float eyeY = this->eye_.y;
-
-        // TODO: Check to make sure cube is really below camera
-        if(eyeY - camH < c.y){
-            this->velocity_.y = 0.0;
-            std::cout << "Eye at" << eyeY << " collision on " << glm::to_string(c) << std::endl;
+    for (const auto& c : cubes) {
+        if (glm::length(c - this->eye_) < coarseRadius) {
+            coarseCollisions.push_back(c);
         }
     }
 
-    // Fine detecion: walls
+    // Fine detection
+    CollisionType allColl = NONE;
+    for (const auto& c : coarseCollisions) {
+        CollisionType coll = collide(this->eye_, c);
 
+        if(coll & FLOOR){
+            this->velocity_.y = max(0.0f, this->velocity_.y);
+            allColl = allColl | FLOOR;
+        }
+        if(coll & CEIL){
+            this->velocity_.y = min(0.0f, this->velocity_.y);
+            allColl = allColl | CEIL;
+        }
+        if(coll & MINX){
+            this->velocity_.x = max(0.0f, this->velocity_.x);
+            allColl = allColl | MINX;
+        }
+        if(coll & MAXX){
+            this->velocity_.x = min(0.0f, this->velocity_.x);
+            allColl = allColl | MAXX;
+        }
+        if(coll & MINZ){
+            this->velocity_.z = max(0.0f, this->velocity_.z);
+            allColl = allColl | FLOOR;
+        }
+        if(coll & MAXZ){
+            this->velocity_.z = min(0.0f, this->velocity_.z);
+            allColl = allColl | FLOOR;
+        }
+
+    }
 
     // Update camera position with velocity + explicit Euler
     this->eye_ += (float)timestep * this->velocity_;
@@ -170,6 +274,7 @@ void Camera::update_physics(double timestep, const Chunk& C, const std::vector<g
     update_internal_data();
 }
 
-glm::vec3 Camera::getEye() const{
+glm::vec3 Camera::getEye() const
+{
     return eye_;
 }
